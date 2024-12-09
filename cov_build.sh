082fd0adbab86423e0b4e5c6278a2659b427821d
madhubabutt <114217841+madhubabutt@users.noreply.github.com>
2024-12-10 04:09:09 +0530

    RDK-52982 [RDKE] Coverity framework integration for dcm-agent (#3)
    Co-authored-by: mtirum011 <madhubabu_tirumala@comcast.com>

---
+++ cov_build.sh
@@ -0,0 +1,44 @@
+##########################################################################
+# If not stated otherwise in this file or this component's LICENSE
+# file the following copyright and licenses apply:
+#
+# Copyright 2024 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+
+
+WORKDIR=`pwd`
+
+## Build and install critical dependency
+export ROOT=/usr
+export INSTALL_DIR=${ROOT}/local
+mkdir -p $INSTALL_DIR
+
+cd $ROOT
+#Build rbus
+
+git clone https://github.com/rdkcentral/rbus
+cmake -Hrbus -Bbuild/rbus -DBUILD_FOR_DESKTOP=ON -DCMAKE_BUILD_TYPE=Debug
+make -C build/rbus && make -C build/rbus install
+
+cd $WORKDIR
+
+export INSTALL_DIR='/usr/local'
+export top_srcdir=`pwd`
+export top_builddir=`pwd`
+
+autoreconf --install
+
+./configure --prefix=${INSTALL_DIR}
+make && make install
