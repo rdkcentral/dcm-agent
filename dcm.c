69ef610b09163559849bae52c8d8a6c6f4d49391
Sahu <satyasundar_sahu@comcast.com>
2024-12-26 15:14:18 +0530

    DELIA-67068: DCM Agent is using difw script
    Reason for change: * replace difw script to swupdate_utils.sh script, Which is
                         called difw script inside. Make sure if maintenance manager is
                         enable then dcm should not schedule difw.
    Test Procedure: Build should pass and L1 Unit Test all Test case should pass
    Risks: Low
    Priority: P1
    
    Signed-off-by: Sahu <satyasundar_sahu@comcast.com>

--- dcm.c
+++ dcm.c
@@ -80,10 +80,10 @@ static VOID dcmRunJobs(const INT8* profileName, VOID *pHandle)
         DCMInfo("\nStart log upload Script\n");
         snprintf(pExecBuff, EXECMD_BUFF_SIZE, "nice -n 19 /bin/busybox sh %s/uploadSTBLogs.sh %s 0 1 0 %s %s &",
                                                pRDKPath, DCM_LOG_TFTP, pPrctl, pURL);
-    }
-    else if(strcmp(profileName, DCM_DIFD_SCHED) == 0) {
+    }// maintenance manager is not enable then schedule firmware update
+    else if((0 == dcmSettingsGetMMFlag()) && (strcmp(profileName, DCM_DIFD_SCHED) == 0)) {
         DCMInfo("Start FW update Script\n");
-        snprintf(pExecBuff, EXECMD_BUFF_SIZE, "/bin/sh %s/deviceInitiatedFWDnld.sh 0 2 >> /opt/logs/swupdate.log 2>&1",
+        snprintf(pExecBuff, EXECMD_BUFF_SIZE, "/bin/sh %s/swupdate_utility.sh 0 2 >> /opt/logs/swupdate.log 2>&1",
                                                pRDKPath);
     }
 
