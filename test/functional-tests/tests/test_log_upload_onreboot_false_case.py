e953bedefe0a63c5a3518bbbe2aab0282e47b732
PriyaDharshini_Kathiravan <priyakathiravan05@gmail.com>
2025-09-02 05:09:10 +0000

    Update test cases for logupload

--- test/functional-tests/tests/test_log_upload_onreboot_false_case.py
+++ test/functional-tests/tests/test_log_upload_onreboot_false_case.py
@@ -38,25 +38,24 @@ def test_upload_cron_present():
 def test_upload_script_started():
     assert "UploadOnReboot=0" in grep_dcmdlogs("Triggered uploadSTBLogs.sh with arguments")
     assert "Called uploadLogOnReboot with false" in grep_dcmdlogs("Called uploadLogOnReboot with false")
+    sleep(420)
 
 @pytest.mark.run(order=3)
 def test_fw_cron_scheduled():
-    sleep(200)
     assert "Scheduling DCM_FW_UPDATE Job handle" in grep_dcmdlogs("Scheduling DCM_FW_UPDATE Job handle")
 
 
 @pytest.mark.run(order=4)
 def test_upload_cron_scheduled():
-    sleep(300)
     assert "Scheduling DCM_LOG_UPLOAD Job handle"  in grep_dcmdlogs("Scheduling DCM_LOG_UPLOAD Job handle")
+    sleep(60)
 
 @pytest.mark.run(order=5)
 def test_upload_script_started():
     assert "Start log upload Script"  in grep_dcmdlogs("Start log upload Script")
     assert "Called uploadDCMLogs" in grep_dcmdlogs("Called uploadDCMLogs")
-    sleep(60)
 
 @pytest.mark.run(order=6)
 def test_fwupdate_script_started():
-    #assert "Starting SoftwareUpdate Utility Script..." in grep_dcmdlogs("Starting SoftwareUpdate Utility Script...")
+    assert "Starting SoftwareUpdate Utility Script..." in grep_dcmdlogs("Starting SoftwareUpdate Utility Script...")
     assert "trigger type=" in grep_dcmdlogs("trigger type=")
