6d9215fbe1e27ec214859d30bca5adc5d0e2f35a
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-10-06 15:40:14 +0530

    Create dcm_cronparse_gtest.cpp

---
+++ unittest/dcm_cronparse_gtest.cpp
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2023 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <cstring>
+#include <stdio.h>
+#include "dcm_cronparse.h"
+/*#include "rdm_types.h"
+#include "rdm.h"
+#include "rdm_utils.h"
+*/
+/*
+  #include "mocks/mock_curl.h"
+extern "C" {
+    #include "rdm_curldownload.h"
+}
+*/
+
+#define GTEST_DEFAULT_RESULT_FILEPATH "/tmp/Gtest_Report/"
+#define GTEST_DEFAULT_RESULT_FILENAME "dcm_cronparse_gtest_report.json"
+#define GTEST_REPORT_FILEPATH_SIZE 256
+
+
+using namespace testing;
+using namespace std;
+using ::testing::_;
+using ::testing::Return;
+using ::testing::SetArgPointee;
+using ::testing::DoAll;
+using ::testing::StrEq;
+
+
+class DCMTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+    }
+
+    void TearDown() override {
+    }
+};
