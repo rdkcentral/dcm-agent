a988c901c5a936aab939718fdfb2c6bdb28417d9
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-10-08 20:06:25 +0530

    Create dcm_schedjob_gtest.cpp

---
+++ unittest/dcm_schedjob_gtest.cpp
@@ -0,0 +1,84 @@
+/**
+ * Copyright 2023 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <cstring>
+#include <stdio.h>
+#include <fstream>
+
+extern "C" {
+#include "dcm_cronparse.h"
+#include "../dcm_types.h"
+#include "dcm_cronparse.c"
+
+}
+#include "../dcm_schedjob.c"
+
+/*#include "rdm_types.h"
+#include "rdm.h"
+#include "rdm_utils.h"
+*/
+/*
+  #include "mocks/mock_curl.h"
+extern "C" {
+    #include "rdm_curldownload.h"
+}
+*/
+
+#define GTEST_DEFAULT_RESULT_FILEPATH "/tmp/Gtest_Report/"
+#define GTEST_DEFAULT_RESULT_FILENAME "dcm_cronparse_gtest_report.json"
+#define GTEST_REPORT_FILEPATH_SIZE 256
+
+
+using namespace testing;
+using namespace std;
+using ::testing::_;
+using ::testing::Return;
+using ::testing::SetArgPointee;
+using ::testing::DoAll;
+using ::testing::StrEq;
+
+//extern static INT32 dcmCronParseToUpper(INT8* str);
+class DCMshedjobTest: public ::testing::Test {
+protected:
+    void SetUp() override {
+    }
+
+    void TearDown() override {
+    }
+};
+
+#include "dcm_schedjob.h"
+
+
+
+GTEST_API_ int main(int argc, char *argv[]){
+    char testresults_fullfilepath[GTEST_REPORT_FILEPATH_SIZE];
+    char buffer[GTEST_REPORT_FILEPATH_SIZE];
+
+    memset( testresults_fullfilepath, 0, GTEST_REPORT_FILEPATH_SIZE );
+    memset( buffer, 0, GTEST_REPORT_FILEPATH_SIZE );
+
+    snprintf( testresults_fullfilepath, GTEST_REPORT_FILEPATH_SIZE, "json:%s%s" , GTEST_DEFAULT_RESULT_FILEPATH , GTEST_DEFAULT_RESULT_FILENAME);
+    ::testing::GTEST_FLAG(output) = testresults_fullfilepath;
+    ::testing::InitGoogleTest(&argc, argv);
+    //testing::Mock::AllowLeak(mock);
+    cout << "Starting DCM GTEST ===================>" << endl;
+    return RUN_ALL_TESTS();
+}
