f4e3b4da9ca7c3cfd553c1c534116f367365a8b4
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-10-11 18:35:44 +0530

    Update dcm_parseconf_gtest.cpp

--- unittest/dcm_parseconf_gtest.cpp
+++ unittest/dcm_parseconf_gtest.cpp
@@ -25,9 +25,10 @@
 #include "./mocks/mockRbus.h"
 
 extern "C" {
-#include "dcm_types.h"
+//#include "dcm_types.h"
 //#include "dcm_parseconf.h"
 }
+#include "dcm_types.h"
 #include "dcm_parseconf.c"
 #define GTEST_DEFAULT_RESULT_FILEPATH "/tmp/Gtest_Report/"
 #define GTEST_DEFAULT_RESULT_FILENAME "dcm_cronparse_gtest_report.json"
