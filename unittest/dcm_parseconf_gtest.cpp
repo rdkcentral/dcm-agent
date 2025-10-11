1a7d2abc93d52ac6e8cb6b0681a71a1016480526
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-10-11 16:57:22 +0530

    Create dcm_parseconf_gtest.cpp

---
+++ unittest/dcm_parseconf_gtest.cpp
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2023 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <cstring>
+#include <stdio.h>
+#include <climits>
+#include <cerrno>
+
+
+extern "C" {
+//#include "dcm_parseconf.c"
+#include "dcm_parseconf.h"
+}
+
+#define GTEST_DEFAULT_RESULT_FILEPATH "/tmp/Gtest_Report/"
+#define GTEST_DEFAULT_RESULT_FILENAME "dcm_cronparse_gtest_report.json"
+#define GTEST_REPORT_FILEPATH_SIZE 256
+
+
+using namespace testing;
+using namespace std;
+using ::testing::_;
+using ::testing::Return;
+using ::testing::SetArgPointee;
+using ::testing::DoAll;
+using ::testing::StrEq;
+
+class dcmCronParseTest : public ::testing::Test {
+protected:
+    void SetUp(){
+    }
+
+    void TearDown(){
+    }
+};
