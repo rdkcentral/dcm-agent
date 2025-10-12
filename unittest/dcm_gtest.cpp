b0f4189011d85f39c435d3064436e490543b25b4
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-10-12 21:46:51 +0530

    Update dcm_gtest.cpp

--- unittest/dcm_gtest.cpp
+++ unittest/dcm_gtest.cpp
@@ -33,7 +33,8 @@
 }*/
 //#include "../dcm_utils.c"
 #include "dcm_types.h"
-//#include "dcm_rbus.c"
+#include "dcm_rbus.c"
+#include "dcm_parseconf.c"
 #include "dcm.c"
 #define GTEST_DEFAULT_RESULT_FILEPATH "/tmp/Gtest_Report/"
 #define GTEST_DEFAULT_RESULT_FILENAME "dcm_cronparse_gtest_report.json"
