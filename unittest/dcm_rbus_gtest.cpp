d53ea0dcb432e250c28b760e7edbf73791a63e45
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-10-12 18:37:31 +0530

    Update dcm_rbus_gtest.cpp

--- unittest/dcm_rbus_gtest.cpp
+++ unittest/dcm_rbus_gtest.cpp
@@ -33,7 +33,7 @@
 }*/
 //#include "../dcm_utils.c"
 #include "dcm_types.h"
-//#include "dcm_parseconf.c"
+#include "dcm_rbus.c"
 #define GTEST_DEFAULT_RESULT_FILEPATH "/tmp/Gtest_Report/"
 #define GTEST_DEFAULT_RESULT_FILENAME "dcm_cronparse_gtest_report.json"
 #define GTEST_REPORT_FILEPATH_SIZE 256
