4b0619cf3c9064a16ce38c3772ab0f2114777c63
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-10-12 11:37:33 +0530

    Update dcm_rbus_gtest.cpp

--- unittest/dcm_rbus_gtest.cpp
+++ unittest/dcm_rbus_gtest.cpp
@@ -8,7 +8,7 @@ extern "C" {
 //#include "mockrbus.h‎"
 //#include "mockrbus.cpp"
 }
-//#include "mockrbus.h‎"
+#include "mockrbus.h‎"
 #include "dcm_rbus.c"
 using ::testing::_;
 using ::testing::Return;
