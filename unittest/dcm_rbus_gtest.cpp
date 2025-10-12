0a337704b89014fb0afa1fa94ad83ad16682f530
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-10-12 18:00:30 +0530

    Update dcm_rbus_gtest.cpp

--- unittest/dcm_rbus_gtest.cpp
+++ unittest/dcm_rbus_gtest.cpp
@@ -1,3 +1,81 @@
+/**
+ * Copyright 2023 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <cstring>
+#include <stdio.h>
+#include <climits>
+#include <cerrno>
+#include <fstream>
+//#include "./mocks/mockRbus.h"
+#include "./mocks/mockrbus.h"
+
+/*extern "C" {
+//#include "dcm_types.h"
+//#include "dcm_parseconf.h"
+
+}*/
+#include "../dcm_utils.c"
+#include "dcm_types.h"
+#include "dcm_parseconf.c"
+#define GTEST_DEFAULT_RESULT_FILEPATH "/tmp/Gtest_Report/"
+#define GTEST_DEFAULT_RESULT_FILENAME "dcm_cronparse_gtest_report.json"
+#define GTEST_REPORT_FILEPATH_SIZE 256
+
+
+using namespace testing;
+using namespace std;
+using ::testing::_;
+using ::testing::Return;
+using ::testing::SetArgPointee;
+using ::testing::DoAll;
+using ::testing::StrEq;
+
+
+void CreateFile(const char* filename, const char* content) {
+    std::ofstream ofs(filename);
+    ofs << content;
+}
+
+void RemoveFile(const char* filename) {
+    std::remove(filename);
+}
+
+void CreateDirectory(const char* dirname) {
+    mkdir(dirname, 0755);
+}
+
+void RemoveDirectory(const char* dirname) {
+    rmdir(dirname);
+}
+
+class dcmRbusTest : public ::testing::Test {
+protected:
+    void SetUp(){
+    }
+
+    void TearDown(){
+
+    }
+};
+
+/*
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
@@ -102,7 +180,7 @@ TEST_F(DcmRbusTest, dcmRbusSendEvent_Success) {
     
     EXPECT_EQ(result, DCM_SUCCESS);
 }
-
+*/
 GTEST_API_ int main(int argc, char *argv[]){
     char testresults_fullfilepath[GTEST_REPORT_FILEPATH_SIZE];
     char buffer[GTEST_REPORT_FILEPATH_SIZE];
