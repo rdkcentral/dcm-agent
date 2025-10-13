28989a5e1e54e04bb4a48ad5958d613ac6a4b16b
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-10-13 23:55:05 +0530

    Update dcm_rbus_gtest.cpp

--- unittest/dcm_rbus_gtest.cpp
+++ unittest/dcm_rbus_gtest.cpp
@@ -500,7 +500,6 @@ TEST_F(RbusProcConfTest, rbusAsyncSubCB_subscrption_null)
     rbusError_t error = RBUS_ERROR_SUCCESS;
     // Call the function
     get_rbusAsyncSubCB(mockHandle, nullptr, error);
-    EXPECT_EQ(dcmRbusHandle->eventSub, 1);
     
 }
 
