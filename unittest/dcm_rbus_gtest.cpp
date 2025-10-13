cab29a9961e4fd1254ac56e17596aaec57f91cdb
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-10-14 01:54:18 +0530

    Update dcm_rbus_gtest.cpp

--- unittest/dcm_rbus_gtest.cpp
+++ unittest/dcm_rbus_gtest.cpp
@@ -146,7 +146,7 @@ TEST_F(DcmRbusTest, dcmRbusUnInit_rbus_event_subscribe_fail) {
        .WillOnce(Return(RBUS_ENABLED));
     
     EXPECT_CALL(*mockRBus, rbus_open(_, _))
-        .WillOnce(DoAll(SetArgPointee<0>(mockHandle), Return(RBUS_ERROR_SUCCESS)));
+        .WillOnce(Return(RBUS_ERROR_SUCCESS));
     
     int result = dcmRbusInit(&handle);
     
