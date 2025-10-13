2ed2d5aef94e07140c155b43b59ccd043c6b0ded
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-10-13 17:30:21 +0530

    Update dcm_gtest.cpp

--- unittest/dcm_gtest.cpp
+++ unittest/dcm_gtest.cpp
@@ -358,7 +358,7 @@ protected:
         // Mock scheduler handles (pointers to indicate they exist)
         g_pdcmHandle->pLogSchedHandle = (VOID*)0x12345678;
         g_pdcmHandle->pDifdSchedHandle = (VOID*)0x87654321;
-        g_pdcmHandle->pRbusHandle = mock_rbus_get_mock_handle();
+        g_pdcmHandle->pRbusHandle = (VOID*)0x87654322;
     }
     
     void cleanupComponents() {
