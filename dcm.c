e6ba30d3636a67109f340bf8625ce3726aaa5e31
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-10-14 14:52:00 +0530

    Update dcm.c

--- dcm.c
+++ dcm.c
@@ -38,7 +38,7 @@
 #include "dcm_cronparse.h"
 #include "dcm_schedjob.h"
 
-//static DCMDHandle *g_pdcmHandle = NULL;
+static DCMDHandle *g_pdcmHandle = NULL;
 DCMDHandle *g_pdcmHandle = NULL;
 /** @brief Call back function from Scheduler. This function
  *         Initiates the job
