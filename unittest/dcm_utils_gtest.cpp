2e0d5441bd2b9b003810acb3dc79439fa3918a0b
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-10-15 01:11:37 +0530

    Update dcm_utils_gtest.cpp

--- unittest/dcm_utils_gtest.cpp
+++ unittest/dcm_utils_gtest.cpp
@@ -27,15 +27,15 @@ extern "C" {
 //#include "dcm_cronparse.c"
 #include "../dcm_types.h"
 
-#include "dcm_cronparse.c"
+
 #include "../dcm_types.h"
-#include "../dcm_utils.c" 
+
 } 
 */
 
 //#include "dcm_cronparse.h"
 #include "../dcm_types.h"
-#include "../dcm_utils.c"
+
 //#include "dcm_utils.h"
 //#include "dcm_cronparse.h"
 //#include "../dcm_types.h"
