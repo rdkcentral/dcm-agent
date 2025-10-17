266ed151a643f6cf33f0557cd1b5ecb4426f78c1
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-10-18 02:26:00 +0530

    Update dcm_gtest.cpp

--- unittest/dcm_gtest.cpp
+++ unittest/dcm_gtest.cpp
@@ -363,7 +363,7 @@ public:  // Make public for test access
 TEST_F(DcmDaemonMainUnInitTest, UnInit_ValidHandle_CompletesSuccessfully) {
     testHandle.isDCMRunning = true;
     EXPECT_CALL(*mockSettings, dcmSettingsInit(_))
-        .WillOnce(Return(DCM_FAILURE));
+        .WillOnce(Return(DCM_SUCCESS));
     EXPECT_NO_THROW(dcmDaemonMainUnInit(&testHandle));
     
     EXPECT_EQ(testHandle.pExecBuff, nullptr);
@@ -377,7 +377,7 @@ TEST_F(DcmDaemonMainUnInitTest, UnInit_DCMNotRunning_RemovesPIDFile) {
     testHandle.isDCMRunning = false;
     createPIDFile();
     EXPECT_CALL(*mockSettings, dcmSettingsInit(_))
-        .WillOnce(Return(DCM_FAILURE));
+        .WillOnce(Return(DCM_SUCCESS));
     EXPECT_TRUE(pidFileExists());
     EXPECT_NO_THROW(dcmDaemonMainUnInit(&testHandle));
 }
