09f17277109059de18f0b74621e46ae2dc0cebc8
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-10-17 14:25:00 +0530

    Update dcm_cronparse.h

--- dcm_cronparse.h
+++ dcm_cronparse.h
@@ -21,6 +21,11 @@
 
 #ifndef _DCM_CRONPARSE_H_
 #define _DCM_CRONPARSE_H_
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #ifdef GTEST_ENABLE
 #include "dcm_types.h"
 #endif
@@ -40,6 +45,9 @@ INT32 dcmCronParseExp(const INT8* expression, dcmCronExpr* target);
 
 time_t dcmCronParseGetNext(dcmCronExpr* expr, time_t date);
 
+#ifdef __cplusplus
+}
+#endif
 #endif //_DCM_CRONPARSE_H_
 
 
