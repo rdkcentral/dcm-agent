435a1a7b99730a2eaf627cc197fae28403c8676e
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-10-18 20:10:43 +0530

    Update dcm_schedjob_gtest.cpp

--- unittest/dcm_schedjob_gtest.cpp
+++ unittest/dcm_schedjob_gtest.cpp
@@ -294,7 +294,7 @@ TEST_F(DcmSchedulerThreadTest, ThreadExecutesCallback_QuickCron) {
     // Wait up to 70 seconds (to account for minute boundary)
     bool callbackReceived = false;
     
-    for (int i = 0; i < 300; i++) { // 70 seconds
+    for (int i = 0; i < 350; i++) { // 70 seconds
         usleep(100000); // 100ms intervals 100000
         if (g_callbackExecuted) {
             callbackReceived = true;
