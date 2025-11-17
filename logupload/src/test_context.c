4d4db377d2baa9d874c2aa7d0b61c1ee25ec0831
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-11-17 21:25:37 +0530

    Update test_context.c

--- logupload/src/test_context.c
+++ logupload/src/test_context.c
@@ -1,27 +1,73 @@
 #include "context.h"
-#include "rdk_logger.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
     Context ctx;
     char errmsg[256];
 
+    printf("Initializing context...\n");
     if (!context_init(&ctx)) {
-        fprintf(stderr, "Context init failed\n");
+        fprintf(stderr, "context_init failed!\n");
         return 1;
     }
+    printf("Validation...\n");
     int v = context_validate(&ctx, errmsg, sizeof(errmsg));
-    printf("Validation=%d (%s)\n", v, errmsg);
-    printf("MAC=%s compact=%s\n", ctx.mac_raw, ctx.mac_compact);
-    printf("LogFile=%s\n", ctx.log_file);
-    printf("DRIFile=%s\n", ctx.dri_log_file);
-    printf("DeviceType=%s Firmware=%s\n", ctx.device_type, ctx.firmware_version);
-    printf("TLS Option=%s UploadFlag=%d UseCodebig=%d\n",
-           ctx.tls_option, ctx.upload_flag, ctx.use_codebig);
+    printf("Validation code: %d (%s)\n", v, errmsg);
 
-    /* Example log usage */
-    RDK_LOG(RDK_LOG_INFO, LOG_UPLOAD , "Test info after initialization\n");
+    printf("\nVerifying parsed context fields:\n");
+    printf("MAC raw        : %s\n", ctx.mac_raw);
+    printf("MAC compact    : %s\n", ctx.mac_compact);
+    //printf("Host IP        : %s\n", ctx.host_ip);
+    printf("Device Type    : %s\n", ctx.device_type);
+    printf("Build Type     : %s\n", ctx.build_type);
 
-    rdk_logger_deinit();
+    printf("log_path       : %s\n", ctx.log_path);
+    printf("dcm_log_path   : %s\n", ctx.dcm_log_path);
+    printf("prev_log_path  : %s\n", ctx.prev_log_path);
+    printf("prev_log_backup: %s\n", ctx.prev_log_backup_path);
+    printf("dcm_upload_list_path : %s\n", ctx.dcm_upload_list_path);
+    printf("dcm_script_log_path  : %s\n", ctx.dcm_script_log_path);
+    printf("tls_error_log_path   : %s\n", ctx.tls_error_log_path);
+    printf("rrd_log_file    : %s\n", ctx.rrd_log_file);
+    printf("curl_info_path  : %s\n", ctx.curl_info_path);
+    printf("http_code_path  : %s\n", ctx.http_code_path);
+    printf("previous_reboot_info_path: %s\n", ctx.previous_reboot_info_path);
+    printf("direct_block_file        : %s\n", ctx.direct_block_file);
+    printf("codebig_block_file       : %s\n", ctx.codebig_block_file);
+    printf("telemetry_path           : %s\n", ctx.telemetry_path);
+    printf("rrd_log_dir              : %s\n", ctx.rrd_log_dir);
+    printf("iarm_event_bin_dir       : %s\n", ctx.iarm_event_bin_dir);
+
+    printf("log_file        : %s\n", ctx.log_file);
+    printf("dri_log_file    : %s\n", ctx.dri_log_file);
+
+    printf("dt_stamp        : %s\n", ctx.dt_stamp);
+    printf("timestamp_long  : %s\n", ctx.timestamp_long);
+
+    printf("enable_ocsp_stapling   : %d\n", ctx.enable_ocsp_stapling);
+    printf("enable_ocsp           : %d\n", ctx.enable_ocsp);
+    printf("encryption_enabled_rfc: %d\n", ctx.encryption_enabled_rfc);
+    printf("upload_flag           : %d\n", ctx.upload_flag);
+    printf("use_codebig           : %d\n", ctx.use_codebig);
+    printf("privacy_block         : %d\n", ctx.privacy_block);
+
+    printf("num_upload_attempts    : %d\n", ctx.num_upload_attempts);
+    printf("cb_num_upload_attempts : %d\n", ctx.cb_num_upload_attempts);
+    printf("curl_tls_timeout       : %d\n", ctx.curl_tls_timeout);
+    printf("curl_timeout           : %d\n", ctx.curl_timeout);
+
+    printf("tr181_unsched_reboot_disable : %d\n", ctx.tr181_unsched_reboot_disable);
+    printf("rrd_tr181_name               : %s\n", ctx.rrd_tr181_name);
+
+    printf("tls_option        : %s\n", ctx.tls_option);
+    printf("cloud_url         : %s\n", ctx.cloud_url);
+
+    printf("log_upload_success_code : %d\n", ctx.log_upload_success_code);
+    printf("log_upload_failed_code  : %d\n", ctx.log_upload_failed_code);
+    printf("log_upload_aborted_code : %d\n", ctx.log_upload_aborted_code);
+
+    printf("\nAll done.\n");
+    context_deinit(&ctx);
     return 0;
 }
