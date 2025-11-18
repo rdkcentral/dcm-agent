e895f0fbd538aba2867508fd1eaa7f1c6b5b515b
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-11-18 22:11:18 +0530

    Update context.h

--- logupload/include/context.h
+++ logupload/include/context.h
@@ -1,3 +1,22 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2018 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 #ifndef CONTEXT_H
 #define CONTEXT_H
 
@@ -67,7 +86,6 @@ typedef struct {
 } Context;
 
 bool context_init(Context *ctx);
-int context_validate(Context *ctx, char *errmsg, size_t errmsg_sz);
 void context_deinit(Context *ctx);
 
 #endif
