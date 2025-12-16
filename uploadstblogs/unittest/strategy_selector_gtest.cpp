88e16154de4753e3dab901422c815b5e4174985a
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-12-16 08:03:02 +0530

    Update strategy_selector_gtest.cpp

--- uploadstblogs/unittest/strategy_selector_gtest.cpp
+++ uploadstblogs/unittest/strategy_selector_gtest.cpp
@@ -13,10 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * SPDX-License-Identifier: Apache-2.0
- */
-/**
- * Copyright 2025 RDK Management
  */
 
 #include <gtest/gtest.h>
