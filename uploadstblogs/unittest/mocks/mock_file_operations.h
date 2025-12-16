e3ae01ff087f28d53a56fe191358286b45941ddd
Valappil, Abhinav (Contractor) <Abhinav_Valappil@comcast.com>
2025-12-16 15:35:31 +0000

    Updating copyright header

--- uploadstblogs/unittest/mocks/mock_file_operations.h
+++ uploadstblogs/unittest/mocks/mock_file_operations.h
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * SPDX-License-Identifier: Apache-2.0
  */
 
 #ifndef MOCK_FILE_OPERATIONS_H
