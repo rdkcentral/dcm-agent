f2f74e4ea3eb95fbd981f3e6575768129664c3b1
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-02-10 16:53:10 +0530

    Update context_manager.c

--- uploadstblogs/src/context_manager.c
+++ uploadstblogs/src/context_manager.c
@@ -37,11 +37,7 @@
 #include "common_device_api.h"
 #endif
 #include "rdk_debug.h"
-#ifndef L2_TEST_ENABLED
-#ifndef GTEST_ENABLE
 #include "rdk_logger.h"
-#endif
-#endif
 #include "rbus_interface.h"
 
 #define DEBUG_INI_NAME "/etc/debug.ini"
@@ -165,8 +161,6 @@ bool init_context(RuntimeContext* ctx)
 {
     // Initialize RDK Logger
     /* Extended initialization with programmatic configuration */
-#ifndef L2_TEST_ENABLED
-#ifndef GTEST_ENABLE
     rdk_logger_ext_config_t config = {
         .pModuleName = "LOG.RDK.UPLOADSTB",     /* Module name */
         .loglevel = RDK_LOG_INFO,                 /* Default log level */
@@ -178,14 +172,7 @@ bool init_context(RuntimeContext* ctx)
     if (rdk_logger_ext_init(&config) != RDK_SUCCESS) {
         printf("UPLOADSTB : ERROR - Extended logger init failed\n");
     }
-#endif
-#endif
-    if (0 == rdk_logger_init(DEBUG_INI_NAME)) {
-        g_rdk_logger_enabled = 1;
-        RDK_LOG(RDK_LOG_INFO, LOG_UPLOADSTB, "[%s:%d] RDK Logger initialized\n", __FUNCTION__, __LINE__);
-    } else {
-        fprintf(stderr, "WARNING: RDK Logger initialization failed, using fallback logging\n");
-    } 
+
     if (!ctx) {
         RDK_LOG(RDK_LOG_ERROR, LOG_UPLOADSTB, "[%s:%d] Context pointer is NULL\n", __FUNCTION__, __LINE__);
         return false;
@@ -526,4 +513,5 @@ void cleanup_context(void)
 
 
 }
+
 
