0596f9af23a7dde16f6429b59d12900393b5b0f2
Vismal S Kumar <ajvismal@yahoo.com>
2026-02-19 15:44:15 +0530

    Update uploadstblogs_types.h

--- uploadstblogs/include/uploadstblogs_types.h
+++ uploadstblogs/include/uploadstblogs_types.h
@@ -60,6 +60,7 @@ typedef enum {
     TRIGGER_CRASH = 3,
     TRIGGER_DEBUG = 4,
     TRIGGER_ONDEMAND = 5
+    TRIGGER_MEMCAPTURE = 6;
 } TriggerType;
 
 /**
@@ -309,4 +310,5 @@ void t2_count_notify(char *marker);
 void t2_val_notify(char *marker, char *val);
 
 #endif /* UPLOADSTBLOGS_TYPES_H */
+
 
