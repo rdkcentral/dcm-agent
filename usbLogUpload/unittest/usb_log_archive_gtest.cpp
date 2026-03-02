7212cbb35186e0c760f083955392ff8a24fe0e9c
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-03-02 12:37:06 +0530

    Update usb_log_archive_gtest.cpp

--- usbLogUpload/unittest/usb_log_archive_gtest.cpp
+++ usbLogUpload/unittest/usb_log_archive_gtest.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 RDK Management
+ * Copyright 2026 RDK Management
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
