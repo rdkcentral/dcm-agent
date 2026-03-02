f159ad99b310d331224c3960bb844a50aa80a46c
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-03-02 09:06:31 +0530

    Remove lcov list command from coverage report
    Remove coverage report listing from unit_test.sh

--- unit_test.sh
+++ unit_test.sh
@@ -124,5 +124,4 @@ if [ "$ENABLE_COV" = true ]; then
     lcov --capture --directory . --output-file coverage.info
     lcov --remove coverage.info '/usr/*' --output-file coverage.info
     lcov --remove coverage.info "${PWD}/*" --output-file coverage.info
-    lcov --list coverage.info
 fi
