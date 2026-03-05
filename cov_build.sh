b4f80b1289d71142b1475df19ea2b0c5c29a6ef7
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-03-05 06:16:42 +0530

    Update cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -55,7 +55,7 @@ cp include/*.h /usr/local/include
 sh  build_inside_container.sh
 
 cd ${ROOT}
-git clone https://github.com/rdkcentral/common_utilities.git -b feature/upload_L2
+git clone https://github.com/rdkcentral/common_utilities.git
 cd common_utilities
 autoreconf -i
 ./configure --enable-rdkcertselector --prefix=${INSTALL_DIR} CFLAGS="-Wno-stringop-truncation -DL2_TEST_ENABLED -DRDK_LOGGER"
