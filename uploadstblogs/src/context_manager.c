f5f8333eb58646fb204ce84f895eec32615af4ff
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-06-12 12:28:22 +0530

    Update context_manager.c

--- uploadstblogs/src/context_manager.c
+++ uploadstblogs/src/context_manager.c
@@ -159,7 +159,7 @@ bool init_context(RuntimeContext* ctx)
     rdk_logger_ext_config_t config = {
         .pModuleName = "LOG.RDK.UPLOADSTB",     /* Module name */
         .loglevel = RDK_LOG_INFO,                 /* Default log level */
-        .output = RDKLOG_OUTPUT_CONSOLE,          /* Output to console (stdout/stderr) */
+        .output = RDKLOG_OUTPUT_FILE,          /* Output to console (stdout/stderr) */
         .format = RDKLOG_FORMAT_WITH_TS,          /* Timestamped format */
         .pFilePolicy = NULL                       /* Not using file output, so NULL */
     };
