87c05441fee52522524db2170b084272d8f24215
nhanasi <navihansi@gmail.com>
2026-06-15 15:30:38 -0400

    Update unit_test.sh

--- unit_test.sh
+++ unit_test.sh
@@ -113,11 +113,10 @@ fi
 
 if [ "$ENABLE_COV" = true ]; then
     echo "********************"
-    echo "**** CAPTURE DCM-AGENT COVERAGE DATA ****"
-    echo "********************"
     echo "Generating coverage report"
-    #lcov --capture --directory . --output-file coverage.info
-    #lcov --remove coverage.info '/usr/*' --output-file coverage.info
-    #lcov --remove coverage.info "${PWD}/*" --output-file coverage.info
-    #lcov --list coverage.info
+    echo "********************"
+    lcov --capture --directory . --output-file coverage.info
+    lcov --remove coverage.info '/usr/*' --output-file coverage.info
+    lcov --remove coverage.info "${PWD}/*" --output-file coverage.info
+    lcov --list coverage.info
 fi
