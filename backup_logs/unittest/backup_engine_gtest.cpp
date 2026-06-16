710d98f0256e9f560904fbbe770f39c08e12b940
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-06-16 18:36:42 +0530

    Update backup_engine_gtest.cpp

--- backup_logs/unittest/backup_engine_gtest.cpp
+++ backup_logs/unittest/backup_engine_gtest.cpp
@@ -761,6 +761,15 @@ TEST_F(BackupEngineTest, FileOperations_EdgeCases) {
 // ================================================================================================
 
 int main(int argc, char **argv) {
+    char testresults_fullfilepath[GTEST_REPORT_FILEPATH_SIZE];
+    char buffer[GTEST_REPORT_FILEPATH_SIZE];
+
+    memset( testresults_fullfilepath, 0, GTEST_REPORT_FILEPATH_SIZE );
+    memset( buffer, 0, GTEST_REPORT_FILEPATH_SIZE );
+
+    snprintf( testresults_fullfilepath, GTEST_REPORT_FILEPATH_SIZE, "json:%s%s" , GTEST_DEFAULT_RESULT_FILEPATH , GTEST_DEFAULT_RESULT_FILENAME);
+    ::testing::GTEST_FLAG(output) = testresults_fullfilepath;
     ::testing::InitGoogleTest(&argc, argv);
+    
     return RUN_ALL_TESTS();
 }
