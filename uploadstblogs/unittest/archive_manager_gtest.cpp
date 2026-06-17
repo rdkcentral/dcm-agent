7ba207518e8d54f65f3a840785970055a908248b
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-06-17 12:31:53 +0530

    Update archive_manager_gtest.cpp

--- uploadstblogs/unittest/archive_manager_gtest.cpp
+++ uploadstblogs/unittest/archive_manager_gtest.cpp
@@ -23,6 +23,10 @@
 #include <stdio.h>
 #include <time.h>
 
+#define GTEST_DEFAULT_RESULT_FILEPATH "/tmp/Gtest_Report/"
+#define GTEST_DEFAULT_RESULT_FILENAME "archive_manager_gtest_report.json"
+#define GTEST_REPORT_FILEPATH_SIZE 256
+
 // Mock RDK_LOG before including other headers
 #ifdef GTEST_ENABLE
 #define RDK_LOG(level, module, ...) do {} while(0)
@@ -671,15 +675,16 @@ TEST_F(ArchiveManagerTest, CollectDriLogs_Success) {
     EXPECT_GE(result, 0);
 }
 
-int main(int argc, char** argv) {
+GTEST_API_ int main(int argc, char *argv[]){
+    char testresults_fullfilepath[GTEST_REPORT_FILEPATH_SIZE];
+    char buffer[GTEST_REPORT_FILEPATH_SIZE];
+
+    memset( testresults_fullfilepath, 0, GTEST_REPORT_FILEPATH_SIZE );
+    memset( buffer, 0, GTEST_REPORT_FILEPATH_SIZE );
+
+    snprintf( testresults_fullfilepath, GTEST_REPORT_FILEPATH_SIZE, "json:%s%s" , GTEST_DEFAULT_RESULT_FILEPATH , GTEST_DEFAULT_RESULT_FILENAME);
+    ::testing::GTEST_FLAG(output) = testresults_fullfilepath;
     ::testing::InitGoogleTest(&argc, argv);
-    int result = RUN_ALL_TESTS();
-    
-    // Ensure global mock is cleaned up
-    if (g_mockFileOperations) {
-        delete g_mockFileOperations;
-        g_mockFileOperations = nullptr;
-    }
-    
-    return result;
+    //testing::Mock::AllowLeak(mock);
+    return RUN_ALL_TESTS();
 }
