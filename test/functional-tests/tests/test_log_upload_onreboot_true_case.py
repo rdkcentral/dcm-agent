d89b76b01886754e141ca14320cda363b47ce8d9
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-07-16 14:50:10 +0530

    Update test_log_upload_onreboot_true_case.py

--- test/functional-tests/tests/test_log_upload_onreboot_true_case.py
+++ test/functional-tests/tests/test_log_upload_onreboot_true_case.py
@@ -26,24 +26,17 @@ import json
 
 @pytest.mark.run(order=1)
 def test_upload_cron_present():
-    assert "urn:settings:LogUploadSettings:UploadSchedule:cron" in grep_dcmdlogs("is present setting cron jobs")
+    kill_dcmd(9)
+    kill_telemetry(9)
+    rbus_set_data("Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.ConfigURL", "string", "https://mockxconf:50050/loguploader3/getT2DCMSettings")
+    run_dcmd()
+    run_telemetry()
+    sleep(420)
 
-@pytest.mark.run(order=3)
+@pytest.mark.run(order=2)
 def test_fw_cron_scheduled():
-    sleep(540)
     assert "Scheduling DCM_FW_UPDATE Job handle" in grep_dcmdlogs("Scheduling DCM_FW_UPDATE Job handle")
 
 @pytest.mark.run(order=4)
-def test_fwupdate_script_started():
-    assert "Start FW update Script"  in grep_dcmdlogs("Start FW update Script")
-   # assert "Starting SoftwareUpdate Utility Script..." in grep_dcmdlogs("Starting SoftwareUpdate Utility Script...")
-   #assert "trigger type=" in grep_dcmdlogs("trigger type=")
-
-@pytest.mark.run(order=4)
-def test_upload_cron_scheduled():
-    assert "Scheduling DCM_LOG_UPLOAD Job handle"  in grep_dcmdlogs("Scheduling DCM_LOG_UPLOAD Job handle")
-
-@pytest.mark.run(order=6)
-def test_upload_started():
-    assert "Start log upload via library API"  in grep_dcmdlogs("Start log upload via library API")
-
+def test_mm_enabled():
+    assert "Maintenance manager enabled device - Cron job schedules for maintenance activities are disabled" in grep_dcmdlogs("Maintenance manager enabled device - Cron job schedules for maintenance activities are disabled")
