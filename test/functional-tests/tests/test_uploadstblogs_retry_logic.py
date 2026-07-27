a2f34460de3d4dd7fda2ab8da289b13ab4789947
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-07-28 04:09:52 +0530

    Update test_uploadstblogs_retry_logic.py

--- test/functional-tests/tests/test_uploadstblogs_retry_logic.py
+++ test/functional-tests/tests/test_uploadstblogs_retry_logic.py
@@ -43,20 +43,6 @@ class TestRetryLogic:
         remove_lock_file()
         kill_uploadstblogs()
 
-    @pytest.mark.order(1)
-    def test_network_failure_detection(self):
-        """Test: Service detects network failure"""
-        # Set invalid upload URL to simulate network failure
-        set_include_property("UPLOAD_HTTPLINK", "http://invalid.server.unreachable:9999")
-        
-        create_test_log_files(count=2)
-        
-        result = run_uploadstblogs()
-        
-        # Check for network failure detection
-        failure_logs = grep_uploadstb_logs_regex(r"fail|error|unable|unreachable|timeout")
-        assert len(failure_logs) > 0, "Network failure should be detected"
-
     @pytest.mark.order(2)
     def test_retry_attempts_count(self):
         """Test: Service retries 3 times for Direct path"""
