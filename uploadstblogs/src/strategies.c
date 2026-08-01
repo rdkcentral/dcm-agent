a5a69f6d3fa8e4a8e8cff74c1dd3b87472e50cab
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-08-01 19:36:29 +0530

    Update strategies.c

--- uploadstblogs/src/strategies.c
+++ uploadstblogs/src/strategies.c
@@ -625,10 +625,7 @@ static int dcm_archive(RuntimeContext* ctx, SessionState* session)
                 "[%s:%d] Failed to create archive\n", __FUNCTION__, __LINE__);
         return -1;
     }
-
-#ifndef L2_TEST_ENABLED
-    sleep(60);
-#endif
+	
     RDK_LOG(RDK_LOG_INFO, LOG_UPLOADSTB, 
             "[%s:%d] DCM: Archive phase complete\n", __FUNCTION__, __LINE__);
 
