d762a8cf6f499e55059f9cbf95a9ceb33d488430
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-08-24 11:39:20 +0530

    Update usb_log_archive_gtest.cpp

--- usbLogUpload/unittest/usb_log_archive_gtest.cpp
+++ usbLogUpload/unittest/usb_log_archive_gtest.cpp
@@ -17,57 +17,169 @@
 #include <gtest/gtest.h>
 #include "usb_log_archive.h"
 #include <string>
+#include <cstring>
 #include <cstdio>
 #include <sys/stat.h>
+#include <fstream>
 
-// Mocks and stubs for dependencies
+/* Controllable mock return values */
+static int g_timestamp_retval;
+static int g_create_archive_retval;
+static int g_copy_file_retval;
+static bool g_generate_archive_name_retval;
+static char g_session_archive_file[256];
+
 extern "C" {
     int get_current_timestamp(char *buf, size_t len) {
-        strncpy(buf, "01/01/26-12:00:00", len-1);
-        buf[len-1] = '\0';
+        if (g_timestamp_retval != 0) {
+            return g_timestamp_retval;
+        }
+        strncpy(buf, "01/01/26-12:00:00", len - 1);
+        buf[len - 1] = '\0';
         return 0;
     }
+
     int copy_file_and_delete(const char *src, const char *dst) {
-        // Simulate successful copy
-        return 0;
+        return g_copy_file_retval;
     }
+
+    int create_archive(RuntimeContext *ctx, SessionState *session, const char *source_dir) {
+        if (g_create_archive_retval == 0 && g_session_archive_file[0] != '\0') {
+            strncpy(session->archive_file, g_session_archive_file,
+                    sizeof(session->archive_file) - 1);
+            session->archive_file[sizeof(session->archive_file) - 1] = '\0';
+        }
+        return g_create_archive_retval;
+    }
+
+    bool generate_archive_name(char *buffer, size_t buffer_size,
+                               const char *mac_address, const char *prefix) {
+        if (!g_generate_archive_name_retval) {
+            return false;
+        }
+        snprintf(buffer, buffer_size, "%s_%s_Logs.tar.gz", prefix, mac_address);
+        return true;
+    }
+
     void RDK_LOG(int level, int module, const char *fmt, ...) {}
 }
 
 class UsbLogArchiveTest : public ::testing::Test {
 protected:
     std::string temp_dir;
+
     void SetUp() override {
         temp_dir = "./test_usb_log_dir";
         mkdir(temp_dir.c_str(), 0777);
+
+        /* Reset all mock controls to success defaults */
+        g_timestamp_retval = 0;
+        g_create_archive_retval = 0;
+        g_copy_file_retval = 0;
+        g_generate_archive_name_retval = true;
+        memset(g_session_archive_file, 0, sizeof(g_session_archive_file));
     }
+
     void TearDown() override {
         rmdir(temp_dir.c_str());
     }
 };
 
 TEST_F(UsbLogArchiveTest, CreateUsbLogArchive_Success) {
-    char archive_path[256] = "./test_usb_log_dir/test_archive.tar.gz";
+    char archive_path[] = "./test_usb_log_dir/test_archive.tar.gz";
     int ret = create_usb_log_archive(temp_dir.c_str(), archive_path, "00:11:22:33:44:55");
     EXPECT_EQ(ret, 0);
 }
 
-TEST_F(UsbLogArchiveTest, CreateUsbLogArchive_InvalidParams) {
-    char archive_path[256] = "./test_usb_log_dir/test_archive.tar.gz";
+TEST_F(UsbLogArchiveTest, CreateUsbLogArchive_NullSourceDir) {
+    char archive_path[] = "./test_usb_log_dir/test_archive.tar.gz";
     EXPECT_EQ(create_usb_log_archive(nullptr, archive_path, "00:11:22:33:44:55"), -1);
+}
+
+TEST_F(UsbLogArchiveTest, CreateUsbLogArchive_NullArchivePath) {
     EXPECT_EQ(create_usb_log_archive(temp_dir.c_str(), nullptr, "00:11:22:33:44:55"), -1);
+}
+
+TEST_F(UsbLogArchiveTest, CreateUsbLogArchive_NullMacAddress) {
+    char archive_path[] = "./test_usb_log_dir/test_archive.tar.gz";
     EXPECT_EQ(create_usb_log_archive(temp_dir.c_str(), archive_path, nullptr), -1);
 }
 
 TEST_F(UsbLogArchiveTest, CreateUsbLogArchive_SourceDirMissing) {
-    char archive_path[256] = "./test_usb_log_dir/test_archive.tar.gz";
-    std::string missing_dir = "./does_not_exist";
-    EXPECT_EQ(create_usb_log_archive(missing_dir.c_str(), archive_path, "00:11:22:33:44:55"), -2);
+    char archive_path[] = "./test_usb_log_dir/test_archive.tar.gz";
+    EXPECT_EQ(create_usb_log_archive("./does_not_exist", archive_path, "00:11:22:33:44:55"), -2);
 }
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    int result = RUN_ALL_TESTS();
 
-    return result;
+TEST_F(UsbLogArchiveTest, CreateUsbLogArchive_SourcePathIsFile) {
+    std::string filepath = temp_dir + "/afile.txt";
+    std::ofstream f(filepath);
+    f << "data";
+    f.close();
+
+    char archive_path[] = "./test_usb_log_dir/test_archive.tar.gz";
+    EXPECT_EQ(create_usb_log_archive(filepath.c_str(), archive_path, "00:11:22:33:44:55"), -2);
+    remove(filepath.c_str());
+}
+
+TEST_F(UsbLogArchiveTest, CreateUsbLogArchive_TimestampFails_UsesFallback) {
+    g_timestamp_retval = -1;
+    char archive_path[] = "./test_usb_log_dir/test_archive.tar.gz";
+    int ret = create_usb_log_archive(temp_dir.c_str(), archive_path, "00:11:22:33:44:55");
+    EXPECT_EQ(ret, 0);
+}
+
+TEST_F(UsbLogArchiveTest, CreateUsbLogArchive_CreateArchiveFails) {
+    g_create_archive_retval = -1;
+    char archive_path[] = "./test_usb_log_dir/test_archive.tar.gz";
+    EXPECT_EQ(create_usb_log_archive(temp_dir.c_str(), archive_path, "00:11:22:33:44:55"), 3);
+}
+
+TEST_F(UsbLogArchiveTest, CreateUsbLogArchive_SessionArchiveFileSet) {
+    strncpy(g_session_archive_file, "session_archive.tar.gz", sizeof(g_session_archive_file) - 1);
+    char archive_path[] = "./test_usb_log_dir/test_archive.tar.gz";
+    int ret = create_usb_log_archive(temp_dir.c_str(), archive_path, "00:11:22:33:44:55");
+    EXPECT_EQ(ret, 0);
+}
+
+TEST_F(UsbLogArchiveTest, CreateUsbLogArchive_FallbackToGenerateArchiveName) {
+    /* session.archive_file stays empty, triggers generate_archive_name path */
+    memset(g_session_archive_file, 0, sizeof(g_session_archive_file));
+    char archive_path[] = "./test_usb_log_dir/test_archive.tar.gz";
+    int ret = create_usb_log_archive(temp_dir.c_str(), archive_path, "00:11:22:33:44:55");
+    EXPECT_EQ(ret, 0);
+}
+
+TEST_F(UsbLogArchiveTest, CreateUsbLogArchive_GenerateArchiveNameFails) {
+    memset(g_session_archive_file, 0, sizeof(g_session_archive_file));
+    g_generate_archive_name_retval = false;
+    char archive_path[] = "./test_usb_log_dir/test_archive.tar.gz";
+    EXPECT_EQ(create_usb_log_archive(temp_dir.c_str(), archive_path, "00:11:22:33:44:55"), 3);
+}
+
+TEST_F(UsbLogArchiveTest, CreateUsbLogArchive_CopyFileAndDeleteFails) {
+    g_copy_file_retval = -1;
+    char archive_path[] = "./test_usb_log_dir/test_archive.tar.gz";
+    EXPECT_EQ(create_usb_log_archive(temp_dir.c_str(), archive_path, "00:11:22:33:44:55"), 3);
+}
+
+TEST_F(UsbLogArchiveTest, CreateUsbLogArchive_CopyFailsWithSessionFile) {
+    strncpy(g_session_archive_file, "test.tar.gz", sizeof(g_session_archive_file) - 1);
+    g_copy_file_retval = -1;
+    char archive_path[] = "./test_usb_log_dir/test_archive.tar.gz";
+    EXPECT_EQ(create_usb_log_archive(temp_dir.c_str(), archive_path, "00:11:22:33:44:55"), 3);
+}
+
+TEST_F(UsbLogArchiveTest, CreateUsbLogArchive_AllNullParams) {
+    EXPECT_EQ(create_usb_log_archive(nullptr, nullptr, nullptr), -1);
+}
+
+TEST_F(UsbLogArchiveTest, CreateUsbLogArchive_EmptySourceDir) {
+    char archive_path[] = "./test_usb_log_dir/test_archive.tar.gz";
+    EXPECT_EQ(create_usb_log_archive("", archive_path, "00:11:22:33:44:55"), -2);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
 
